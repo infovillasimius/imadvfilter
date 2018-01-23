4a5571a233590ee4701c62997c16011a94cca78a
Antonello <infovillasimius@gmail.com>
2018-01-23 23:03:25 +0100

    Updated tests

--- time2.m
+++ time2.m
@@ -1,10 +1,10 @@
 clear
 close all
-I=imread ('x-ray.tif');
+I=imread ('lenaBW.tif');
 R=imnoise(I,'gaussian',0,0.002);
-RR=imadvfilter2(R,15,0.002,1.5);
+RR=imadvfilter2(R,9,0.003,1.5);
 figure,imshow(RR);
 
-J = imnoise(I,'gaussian',0,0.002);
-K = wiener2(J,[7 7]);
-figure,imshow(K);
+% J = imnoise(I,'gaussian',0,0.002);
+% K = wiener2(J,[7 7]);
+% figure,imshow(K);
